9b5294192709cd7b38de672eaac902624d6c568b
Christian <xexuDj@gmail.com>
2013-05-04 13:25:44 +0200

    first commit

---
+++ csvreader.js
@@ -0,0 +1,116 @@
+csvreader = {};
+csvreader.Reader = function(separator, mode){
+	this.mode = (mode == "cols") ? mode : "rows";
+	this.separator = separator;
+	this.type = "string";
+	this.parser = undefined;
+	this.as = function(type){
+		switch(type){
+			case "int":
+				this.parser = function(x){
+					if (isNaN(x)){
+						return undefined;
+					} else {
+						return parseInt(x);
+					}
+				}
+				break;
+			case "float":
+				this.parser = function(x){
+					if (isNaN(x)){
+						return undefined;
+					} else {
+						return parseFloat(x);
+					}
+				}
+				break;
+			case "string":
+			default:
+				this.parser = undefined;
+				break;
+		}
+		return this;
+	}
+	this.LoadOnRows = function(csv_string, has_headers){
+		var headers = {},
+			csv = [],
+			mode = "rows";
+		has_headers = has_headers || false;
+		var csv_body = csv_string.split("\n");
+		if(has_headers){
+			var csv_header = csv_body.splice(0,1);
+			csv_header = csv_header[0].split(separator);
+			for(var i = 0;i<csv_header.length;i++){
+				headers[csv_header[i]] = i;
+			}
+		}
+		for(var i = 0;i<csv_body.length;i++){
+			var row = csv_body[i].split(separator);
+			if(this.parser != undefined){
+				for(var j = 0;j<row.length;j++){
+					row[j] = this.parser(row[j]);
+				}
+			}
+			csv.push(row);
+		}
+		return new csvreader.File(mode,csv,has_headers,headers);
+	};
+	this.LoadOnCols = function(csv_string, has_headers){
+		var headers = {},
+			csv = [],
+			cols = 0,
+			mode = "cols";
+		has_headers = has_headers || false;
+		var csv_body = csv_string.split("\n");
+		if(has_headers){
+			var csv_header = csv_body.splice(0,1);
+			csv_header = csv_header[0].split(separator);
+			for(var i = 0;i<csv_header.length;i++){
+				headers[csv_header[i]] = i;
+			}
+			cols = csv_header.length;
+		} else {
+			cols = csv_body[0].length;
+		}
+		for(var i = 0;i<cols;i++){
+			csv.push([]);
+		}
+		for(var i = 0;i<csv_body.length;i++){
+			var row = csv_body[i].split(separator);
+			if(this.parser != undefined){
+				for(var j = 0;j<cols;j++){
+					csv[j].push(this.parser(row[j]));
+				}
+			} else {
+				for(var j = 0;j<cols;j++){
+					csv[j].push(row[j]);
+				}
+			}
+		}
+		return new csvreader.File(mode,csv,has_headers,headers);
+	};
+	this.Load = (this.mode == "rows") ? this.LoadOnRows : this.LoadOnCols;
+};
+csvreader.File = function(mode, content, has_headers, headers){
+	this.has_headers = has_headers;
+	this.headers = (this.has_headers) ? headers : {};
+	this.mode = mode;
+	this.content = content;
+
+	this.get = function(i,j){
+		var index_i = i;
+		var index_j = j;
+		if(this.has_headers){
+			if(this.mode == "rows" && typeof j === "string"){
+				index_j = this.headers[j];
+			} else if(this.mode == "cols" && typeof i === "string"){
+				index_i = this.headers[i];
+			}
+		}
+		if(index_i == undefined || index_j == undefined){
+			return undefined;
+		}
+		var element = this.content[index_i][j];
+		return element;
+	};
+};
---
+++ readme.md
@@ -0,0 +1,48 @@
+# csvreader.js
+It's, or at least aims to be, a simple lightweight lib to parse a string containing the csv itself to a structure you can query by row, column or coordinate.
+It's simple (yet powerful and flexible?), and it means it fits simple needs such as csv files with/without headers, without quotes, without comments and without escape sequences.
+It can be used in the browser or in nodejs without any change.
+
+## Usage
+````javascript
+var separator = ",",
+	mode = "rows", //"rows" or "cols"
+	datatype = "int",
+	input = "0,-489\n1,-485",
+	has_headers = false,
+	reader, csv, item, teststr, testint;
+
+reader =  new csvreader.Reader(separator,mode).as(datatype);
+csv = reader.Load(input,has_headers);
+item = csv.get(0,1);
+
+teststr = item === "-489"; //false!
+testint = item === -489; //true!
+````
+
+## Documentation
+
+````javascript
+csvreader.Reader(separator,mode)
+````
+has default datatype "string", no need for .as("string")
+
+````javascript
+csv.get(0,1);
+````
+will return row 0, col 1 when csv is loaded in mode "rows", or col 0, row 1 when csv is loaded in mode "cols".
+`csv.content[0]` represents the first column of the csv file in "cols" mode.
+`csv.content[0]` represents the first row of the csv file in "cols" mode.
+When using `has_headers = true` you can query using column name, for example:
+````javascript
+csv.get(0,"colA"); //"rows" mode
+csv.get("colA",0); //"cols" mode
+````
+
+## Future
+- Add support to use `.as(function)` to create custom parsers. (very little changes required).
+- Add support to use  `csv.to("rows")` and  `csv.to("cols")` to change the mode.
+
+## License
+As far as it's useful for you use it and modify it to fit your project, if you like it please drop a line to @xexuDj on twitter!
+Christian Córdoba
